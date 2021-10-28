9e80dc4cb159c62ee58b41f27c0fd72120cdb073
plasticpontechnologies <plasticontechnologies@gmail.com>
2021-10-28 16:55:47 +0530

    Second commit

--- src/UsersFolder/Home.js
+++ src/UsersFolder/Home.js
@@ -13,19 +13,28 @@ class Home extends Component {
   render() {
     return (
       <div className="coooo">
-      
-       <div className="imr">
+        <div className="imr">
           <img
             src={image}
-            style={{ paddingTop: "5px", width: "350px", height: "150px", marginTop:"20px" }}
+            style={{
+              paddingTop: "5px",
+              width: "350px",
+              height: "150px",
+              marginTop: "20px",
+            }}
           />
         </div>
-        <section style={{paddingTop:"150px"}}>
-        <div className="pep" style={{paddingTop:"20px"}}>
-      
-        <a href="/UserLogin" className="butt butt2">Login with User</a>    
-        {/* <a href="/" className="butt butt2">Login with Admin</a>     */}
-        </div>    
+        <section style={{ paddingTop: "150px" }}>
+          <div className="pep" style={{ paddingTop: "20px" }}>
+            <a href="/UserLogin" className="butt butt2">
+              Login with User
+            </a>
+            <a href="/Adminsiva" className="butt butt2">
+              Login with Admin
+            </a>
+
+            {/* <a href="/" className="butt butt2">Login with Admin</a>     */}
+          </div>
         </section>
       </div>
     );
